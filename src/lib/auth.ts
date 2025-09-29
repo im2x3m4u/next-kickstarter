8536ce048866572a19358b47b44330a8e5729f79
Setyakc20 <setyakristendy@gmail.com>
2025-09-29 10:46:25 +0700

    revisi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -109,12 +109,6 @@ export const logout = async (): Promise<{ ok: boolean; status: number; message: 
   }
 };
 
-// server side
-import getServerSession from "next-auth"; 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; 
 
-export async function getAuthSession() {
-  return await getServerSession(authOptions);
-}
 
 
---
+++ src/lib/useServer.ts
@@ -0,0 +1,7 @@
+// server side
+import getServerSession from "next-auth"; 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"; 
+
+export async function getAuthSession() {
+  return await getServerSession(authOptions);
+}
