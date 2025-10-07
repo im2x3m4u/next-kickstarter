819b8e2e08f7ddb668c3a2ecbf4f2335b83053ee
dadayan1234 <slasher001@outlook.com>
2025-10-07 10:07:22 +0700

    add : ssl certs for online db and connect to online db

--- src/lib/typeorm.ts
+++ src/lib/typeorm.ts
@@ -5,6 +5,8 @@ import { User } from "../entities/user";
 import { Role } from "../entities/role";
 import { UserRole } from "../entities/userRole";
 import { Activity } from "@/entities/activity";
+import * as fs from "fs"; // <-- Tambahkan import ini
+import * as path from "path"; 
 
 import { Permission } from "@/entities/permission";
 import { RolePermission } from "@/entities/rolePermission";
@@ -13,10 +15,14 @@ import { RolePermission } from "@/entities/rolePermission";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT! || 3306),
+  port: Number(process.env.DB_PORT! || 16903),
   username: process.env.DB_USER!,
   password: process.env.DB_PASS!,
-  database: process.env.DB_NAME!,
+  database: process.env.DB_NAME!,   
+  ssl: {
+    // Baca file ca.pem yang sudah diunduh
+    ca: fs.readFileSync(path.join(process.cwd(), "src/certs/ca.pem")).toString(),
+  },
   synchronize: false,
   logging: false,
   entities: [User, Role, UserRole,Activity, Permission, RolePermission],
